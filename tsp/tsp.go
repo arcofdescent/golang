f5105ef7b0f55c2eb02664559443a22744d10753
Rohan Almeida <almeida@aarohan.biz>
2017-01-31 18:14:28 +0530

    problems with append

--- tsp/tsp.go
+++ tsp/tsp.go
@@ -22,27 +22,110 @@ var distance = map[string]float32{
 
 func main() {
 
-	var dst float32
-	var min float32
-	var minCity string
+	fmt.Println()
 
-	for _, c := range cities {
-		dst = calcDistance(c)
-		fmt.Printf("Origin: %s, Distance: %.2f\n", c, dst)
+	/*
+		vals := [][]int{}
+		res := []int{0, 1, 2}
+		vals = append(vals, res)
+		res = []int{0, 2, 1}
+		vals = append(vals, res)
+		fmt.Printf("%v\n", vals)
+	*/
 
-		if min == 0 {
-			min = dst
-			minCity = c
+	vals := permutations([]int{0, 1, 2}, 3)
+	fmt.Printf("%v\n", vals)
+
+	/*
+		var dst float32
+		var min float32
+		var minCity string
+
+		for _, c := range cities {
+			dst = calcDistance(c)
+			fmt.Printf("Origin: %s, Distance: %.2f\n", c, dst)
+
+			if min == 0 {
+				min = dst
+				minCity = c
+			}
+
+			if min > 0 && dst < min {
+				min = dst
+				minCity = c
+			}
 		}
 
-		if min > 0 && dst < min {
-			min = dst
-			minCity = c
-		}
+		fmt.Println()
+		fmt.Printf("Origin city should be %s\n", minCity)
+	*/
+}
+
+func permutations(iterable []int, r int) [][]int {
+
+	values := [][]int{}
+
+	pool := iterable
+	n := len(pool)
+
+	if r > n {
+		return values
 	}
 
-	fmt.Println()
-	fmt.Printf("Origin city should be %s\n", minCity)
+	indices := make([]int, n)
+	for i := range indices {
+		indices[i] = i
+	}
+
+	cycles := make([]int, r)
+	for i := range cycles {
+		cycles[i] = n - i
+	}
+
+	result := make([]int, r)
+	for i, el := range indices[:r] {
+		result[i] = pool[el]
+	}
+
+	fmt.Println(result)
+	values = append(values, result)
+	fmt.Printf("values: %v\n", values)
+
+	for n > 0 {
+		i := r - 1
+		for ; i >= 0; i -= 1 {
+			cycles[i] -= 1
+			if cycles[i] == 0 {
+				index := indices[i]
+				for j := i; j < n-1; j += 1 {
+					indices[j] = indices[j+1]
+				}
+				indices[n-1] = index
+				cycles[i] = n - i
+			} else {
+				j := cycles[i]
+				indices[i], indices[n-j] = indices[n-j], indices[i]
+
+				for k := i; k < r; k += 1 {
+					result[k] = pool[indices[k]]
+				}
+
+				fmt.Printf("result: %v\n", result)
+				fmt.Printf("values before append: %v\n", values)
+				values = append(values, result)
+				fmt.Printf("values: %v\n", values)
+
+				break
+			}
+		}
+
+		if i < 0 {
+			return values
+		}
+
+	}
+
+	return values
 }
 
 func calcDistance(city string) float32 {
